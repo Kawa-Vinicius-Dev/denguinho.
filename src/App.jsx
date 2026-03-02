1458025f4cffb2bd68844f0e8be3d309bb9a5c60
Kawa Vinicius <kawavinicioss@gmail.com>
2026-03-02 14:07:36 -0300

    Initial commit - estrutura inicial

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen w-full bg-black text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Desafio Casal</h1>
+    </div>
+  )
+}
